65adc53a366ddb61270e779b4339fa1fe9de5ee3
Jeremy Herb <jeremys387@gmail.com>
2021-04-10 11:22:46 -0400

    Responsive, extra css touches
    Mobile up responsive working
    updated some minor css items hovers etc.

--- gulpfile.js
+++ gulpfile.js
@@ -6,45 +6,38 @@ const terser = require('gulp-terser');
 const browsersync = require('browser-sync').create();
 
 // Sass Task
-function scssTask(){
-  return src('src/scss/main.scss', { sourcemaps: true })
-    .pipe(sass())
-    .pipe(postcss([cssnano()]))
-    .pipe(dest('dist/css', { sourcemaps: '.' }));
+function scssTask() {
+	return src('src/scss/main.scss', { sourcemaps: true })
+		.pipe(sass())
+		.pipe(postcss([ cssnano() ]))
+		.pipe(dest('dist/css', { sourcemaps: '.' }));
 }
 
 // JavaScript Task
-function jsTask(){
-  return src('src/js/main.js', { sourcemaps: true })
-    .pipe(terser())
-    .pipe(dest('dist/js', { sourcemaps: '.' }));
+function jsTask() {
+	return src('src/js/main.js', { sourcemaps: true }).pipe(terser()).pipe(dest('dist/js', { sourcemaps: '.' }));
 }
 
 // Browsersync Tasks
-function browsersyncServe(cb){
-  browsersync.init({
-    server: {
-      baseDir: '.'
-    }
-  });
-  cb();
+function browsersyncServe(cb) {
+	browsersync.init({
+		server : {
+			baseDir : '.'
+		}
+	});
+	cb();
 }
 
-function browsersyncReload(cb){
-  browsersync.reload();
-  cb();
+function browsersyncReload(cb) {
+	browsersync.reload();
+	cb();
 }
 
 // Watch Task
-function watchTask(){
-  watch('*.html', browsersyncReload);
-  watch(['src/scss/**/*.scss', 'src/js/**/*.js'], series(scssTask, jsTask, browsersyncReload));
+function watchTask() {
+	watch('*.html', browsersyncReload);
+	watch([ 'src/scss/**/*.scss', 'src/js/**/*.js' ], series(scssTask, jsTask, browsersyncReload));
 }
 
 // Default Gulp task
-exports.default = series(
-  scssTask,
-  jsTask,
-  browsersyncServe,
-  watchTask
-);
+exports.default = series(scssTask, jsTask, browsersyncServe, watchTask);
